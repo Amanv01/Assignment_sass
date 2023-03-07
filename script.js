598f64c3cebf3e951ebe954365b87399c0b93785
amanv01 <aman.verma@netprophetsglobal.com>
2023-03-07 16:12:04 +0530

    all files added

---
+++ script.js
@@ -0,0 +1,50 @@
+function myFunction1() {
+  let x = document.getElementById("hide_box-1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction2() {
+  let x = document.getElementById("hide_box-2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction3() {
+  let x = document.getElementById("hide_box-3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction4() {
+  let x = document.getElementById("hide_box-4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction5() {
+  let x = document.getElementById("hide_box-5");
+  let y = document.getElementById("icon_open");
+  let z = document.getElementById("icon_close");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "block";
+  } else {
+    x.style.display = "none";
+    y.style.display = "none";
+    z.style.display = "block";
+  }
+}
